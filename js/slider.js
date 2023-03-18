c9bd8b6841691970e4141527e8e1d1623e16216f
Sourav Sardar <soourav8@gmail.com>
2023-03-18 15:22:46 +0530

    first-commit

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,23 @@
 const images = [
-    'images/pic-1.jpg', // 0
-    'images/pic-2.jpg', // 1
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
     'images/pic-3.jpg',
     'images/pic-4.jpg',
     'images/pic-5.jpg',
     'images/pic-6.jpg',
     'images/pic-7.jpg',
     'images/pic-8.jpg',
-    'images/pic-9.jpg', // 8
+    'images/pic-9.jpg'
 ]
-
-let imgIndex = 0;
-const imgEl = document.getElementById('slider-img');
-setInterval( () => {
-    if(imgIndex === images.length){
-        imgIndex = 0;
+let imageIndex = 0;
+const imageElement = document.querySelector('#slider-img')
+setInterval(() => {
+    if(imageIndex === images.length){
+        imageIndex = 0;
     }
-    const imgUrl = images[imgIndex];
-    // console.log(imgIndex, imgUrl);
-    imgEl.setAttribute('src', imgUrl);
-    imgIndex++;
-} , 1000)
+    const imgUrl = images[imageIndex];
+    console.log(imageIndex, imgUrl);
+    imageElement.setAttribute('src', imgUrl);
+
+    imageIndex++;
+}, 1000)
